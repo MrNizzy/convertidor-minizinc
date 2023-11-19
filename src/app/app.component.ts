7748d994de630acfb5c33f2f1b82fa71f32442a6
MrNizzy <mr.nizzy.0128@gmail.com>
2023-11-19 21:55:31 +0000

    Update dependencies and providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 interface CantidadElementos {
   productos: number;
@@ -39,6 +40,8 @@ export class AppComponent {
   productos: Producto[] = [];
   materias_primas: MateriaPrima[] = [];
 
+  constructor(private toastr: ToastrService) {}
+
   /**
    * Muestra un ejemplo de código en la variable "codigo".
    */
@@ -50,110 +53,117 @@ export class AppComponent {
    * Genera el código de MiniZinc correspondiente a las restricciones y variables definidas.
    */
   generarMinizinc(): void {
-    this.productos = [];
-    this.materias_primas = [];
-    this.conversion = '';
-    // TODO: Separa los primeros dos valores con salto de línea y guardalos en code.
-    this.code = this.codigo.split('\n');
-    this.cantidadElementos.productos = parseInt(this.code[0]);
-    this.cantidadElementos.materias_primas = parseInt(this.code[1]);
-    if (
-      !this.cantidadElementos.productos ||
-      !this.cantidadElementos.materias_primas
-    ) {
-      this.conversion = 'Error en la cantidad de productos o materias primas';
-      return;
-    }
-    // TODO: Elimina los dos primeros valores de code, luego la cantidad de productos se agregaran a la variable productos desde code iterando en cantidadElementos.productos.
-    this.code.splice(0, 2);
-    this.productos = [];
-    for (let i = 0; i < this.cantidadElementos.productos; i++) {
-      let tempVariables = this.code[0].split(' ');
-      this.productos.push({
-        nombre_variable: tempVariables[0],
-        precio_venta: parseInt(tempVariables[1]),
-        precio_neto: 0,
-        materias_primas: [],
-      });
-      // Guardar el resto de code[0].split() en productos[i].materias_primas
-      tempVariables.splice(0, 2);
-      // Convertir los valores de tempVariables a enteros
-      let tempEnteros = tempVariables.map((e) => parseInt(e));
-
-      this.productos[i].materias_primas = tempEnteros;
-
-      this.code.splice(0, 1);
-    }
-
-    this.code.map((e) => {
-      let tempVariables = e.split(' ');
-      this.materias_primas.push({
-        nombre_variable: tempVariables[0],
-        precio: parseInt(tempVariables[1]),
-        cantidad_max: parseInt(tempVariables[2]),
-      });
-    });
-
-    if (
-      this.productos.length != this.cantidadElementos.productos ||
-      this.materias_primas.length != this.cantidadElementos.materias_primas
-    ) {
-      this.conversion = 'Error en la cantidad de productos o materias primas';
-      return;
-    }
-
-    // agregar precio neto, al precio de venta se le resta el costo de las materias por producto
-    this.productos.map((e) => {
-      e.precio_neto = e.precio_venta;
-      for (let i = 0; i < e.materias_primas.length; i++) {
-        e.precio_neto -= this.materias_primas[i].precio * e.materias_primas[i];
+    try {
+      this.productos = [];
+      this.materias_primas = [];
+      this.conversion = '';
+      // TODO: Separa los primeros dos valores con salto de línea y guardalos en code.
+      this.code = this.codigo.split('\n');
+      this.cantidadElementos.productos = parseInt(this.code[0]);
+      this.cantidadElementos.materias_primas = parseInt(this.code[1]);
+      if (
+        !this.cantidadElementos.productos ||
+        !this.cantidadElementos.materias_primas
+      ) {
+        this.conversion = 'Error en la cantidad de productos o materias primas';
+        this.toastr.warning('Error en la cantidad de productos o materias primas', 'Advertencia')
+        return;
       }
-    });
-
-    this.conversion += `var int: gananciaTotal = `;
-
-    for (let i = 0; i < this.productos.length; i++) {
-      if (i != 0) this.conversion += ` + `;
-      this.conversion += `${this.productos[i].precio_neto} * ${this.productos[i].nombre_variable}`;
-    }
-
-    this.conversion += `;\n\n`;
-
-    this.productos.map((e) => {
-      this.conversion += `var int: ${e.nombre_variable};\n`;
-    });
-
-    this.conversion += `\n`;
-
-    this.productos.map((e) => {
-      this.conversion += `constraint ${e.nombre_variable} >= 0;\n`;
-    });
-
-    this.conversion += `\n`;
-
-    // TODO: Agregar restricciones de cantidad de materias primas
-    for (let i = 0; i < this.materias_primas.length; i++) {
-      this.conversion += `constraint `;
-      for (let j = 0; j < this.productos.length; j++) {
-        if (j != 0) this.conversion += ` + `;
-        this.conversion += `${this.productos[j].materias_primas[i]} * ${this.productos[j].nombre_variable}`;
+      // TODO: Elimina los dos primeros valores de code, luego la cantidad de productos se agregaran a la variable productos desde code iterando en cantidadElementos.productos.
+      this.code.splice(0, 2);
+      this.productos = [];
+      for (let i = 0; i < this.cantidadElementos.productos; i++) {
+        let tempVariables = this.code[0].split(' ');
+        this.productos.push({
+          nombre_variable: tempVariables[0],
+          precio_venta: parseInt(tempVariables[1]),
+          precio_neto: 0,
+          materias_primas: [],
+        });
+        // Guardar el resto de code[0].split() en productos[i].materias_primas
+        tempVariables.splice(0, 2);
+        // Convertir los valores de tempVariables a enteros
+        let tempEnteros = tempVariables.map((e) => parseInt(e));
+  
+        this.productos[i].materias_primas = tempEnteros;
+  
+        this.code.splice(0, 1);
       }
-      this.conversion += ` <= ${this.materias_primas[i].cantidad_max};\n`;
+  
+      this.code.map((e) => {
+        let tempVariables = e.split(' ');
+        this.materias_primas.push({
+          nombre_variable: tempVariables[0],
+          precio: parseInt(tempVariables[1]),
+          cantidad_max: parseInt(tempVariables[2]),
+        });
+      });
+  
+      if (
+        this.productos.length != this.cantidadElementos.productos ||
+        this.materias_primas.length != this.cantidadElementos.materias_primas
+      ) {
+        this.conversion = 'Error en la cantidad de productos o materias primas';
+        this.toastr.warning('Error en la cantidad de productos o materias primas', 'Advertencia')
+        return;
+      }
+  
+      // agregar precio neto, al precio de venta se le resta el costo de las materias por producto
+      this.productos.map((e) => {
+        e.precio_neto = e.precio_venta;
+        for (let i = 0; i < e.materias_primas.length; i++) {
+          e.precio_neto -= this.materias_primas[i].precio * e.materias_primas[i];
+        }
+      });
+  
+      this.conversion += `% Declaración de variables\nvar int: gananciaTotal = `;
+  
+      for (let i = 0; i < this.productos.length; i++) {
+        if (i != 0) this.conversion += ` + `;
+        this.conversion += `${this.productos[i].precio_neto} * ${this.productos[i].nombre_variable}`;
+      }
+  
+      this.conversion += `;\n\n`;
+  
+      this.productos.map((e) => {
+        this.conversion += `var int: ${e.nombre_variable};\n`;
+      });
+  
+      this.conversion += `\n% Restricciones\n`;
+  
+      this.productos.map((e) => {
+        this.conversion += `constraint ${e.nombre_variable} >= 0;\n`;
+      });
+  
+      this.conversion += `\n`;
+  
+      // TODO: Agregar restricciones de cantidad de materias primas
+      for (let i = 0; i < this.materias_primas.length; i++) {
+        this.conversion += `constraint `;
+        for (let j = 0; j < this.productos.length; j++) {
+          if (j != 0) this.conversion += ` + `;
+          this.conversion += `${this.productos[j].materias_primas[i]} * ${this.productos[j].nombre_variable}`;
+        }
+        this.conversion += ` <= ${this.materias_primas[i].cantidad_max};\n`;
+      }
+  
+      this.conversion += `\n% Maximizar las ganancias\nsolve maximize gananciaTotal;\n\n`;
+      this.productos.map((e) => {
+        this.conversion += `output ["${e.nombre_variable} = ", show(${e.nombre_variable}), "\\n"];\n`;
+      });
+      this.conversion += `output ["Ganancia total = ", show(gananciaTotal)];`;
+    } catch {
+      this.conversion = '';
     }
-
-    this.conversion += `\nsolve maximize gananciaTotal;\n\n`;
-    this.productos.map((e) => {
-      this.conversion += `output ["${e.nombre_variable} = ", show(${e.nombre_variable}), "\\n"];\n`;
-
-    });
-    this.conversion += `output ["Ganancia total = ", show(gananciaTotal)];`;
-
-    console.table(this.materias_primas);
-    console.table(this.cantidadElementos);
-    console.table(this.productos);
   }
 
   copyToClipboard(text: string): void {
     navigator.clipboard.writeText(text);
+    this.toastr.success('Copiado al portapapeles', 'Copiado', {
+      timeOut: 2500,
+      progressBar: true,
+      progressAnimation: 'increasing',
+      positionClass: 'toast-top-left',
+    });
   }
 }
